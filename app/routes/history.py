b7eda24f704847c4d354cb6e97c9ec2960ab0ac1
Jiphin George <jiphingeorge80@gmail.com>
2026-07-04 02:55:45 +0530

    feat: implement topbar navigation component and history telemetry search route

--- app/routes/history.py
+++ app/routes/history.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request
 from flask_login import login_required
 
 history_bp = Blueprint('history', __name__)
